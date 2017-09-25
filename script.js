38ffd6445996a89d3275ee54ecf2b50a7bc956ac
Eva <eva.tkavc@gmail.com>
2017-09-25 15:15:37 +0100

    Added the files.

---
+++ script.js
@@ -0,0 +1,21 @@
+//$(window).scroll(function() {
+//  var scrolledY = $(window).scrollTop();
+//  $('#container').css('background-position', 'left ' + ((scrolledY)) + 'px');
+//});
+
+
+<script type="text/javascript">
+  $.backstretch("resources/img/PR_Christmas_Lookbook_15.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_153.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_154.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_155.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_156.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_157.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_158.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_159.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_1510.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_1511.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_1512.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_1513.jpg", {speed: 150});
+  $.backstretch("resources/img/PR_Christmas_Lookbook_1514.jpg", {speed: 150});
+</script>
